d2640775a826acbaa06b990709470f5b386a621a
Neethu Nath <neethu1027@gmail.com>
2020-12-08 00:02:32 +0530

    data type maipulation func and other operations

--- Pandas_Basics_2.py
+++ Pandas_Basics_2.py
@@ -6,22 +6,26 @@ Created on Fri Dec  4 21:22:30 2020
 """
 import pandas as pd
 import os
+import numpy as np
 
 os.chdir(r'D:\GITHUB\PANDAS\Pandas')
 os.listdir()
 #read csv, delimiter is ','
 df1 = pd.read_csv('example.csv',sep=",")
-df1.head()
+df1.head()#default is 5 rows
+df1.head(10)
 #to display column lables 
 df1.columns
+df1.size#number of elelments in df
 df1['Description']
+df1.memory_usage()
 
 #to replace junk values and to set index column
 df11 = pd.read_csv('example.csv',index_col=0 ,na_values=["##","???"])
 #to display row lables 
 df11.index
                                                          
-#import excel sheet
+#import excel sheet and remove special characters
 df2 = pd.read_excel('example.xlsx',sheet_name='sheet1',na_values=["##","???"])
 
                                                                   
@@ -33,5 +37,46 @@ df3_1 = pd.read_table('example.txt', delimiter= "\t")
 #2 ways to copy
 #shallow copy
 sc  = df3.copy(deep=False)
-#deep copy
+#deep copy; 
 dc  = df3.copy(deep=True)
+#slicing and indexing 
+#at-->label-based lookup
+#iat-->integer-based lookup
+df1.at[4,'Revised']#4th row , Revised column data
+df1.iat[4,2]#4th row, 2nd column
+#to access groups of rows and columns by label .loc[]
+df1.loc[2:10,'Period'] #row index from 2nd to 10th of 'Period' column
+
+
+#DATA TYPES
+df1.dtypes
+df1.get_dtype_counts()#group by dtypes and return count of columns
+df1.select_dtypes(exclude=[object])#to select specific data type of columns
+df1.select_dtypes(exclude=[object]).columns
+df1.select_dtypes(include=[object])
+df1.select_dtypes(include=[object]).columns
+df1.info()#summary of dataframe
+#unique/distinct values of particular column
+np.unique(df1['Period'])
+pd.unique(pd.Series(df1['Period']))
+pd.unique(df1['Period'])
+#convert datatype of the column
+df1_test = df1
+df1_test['Revised'] = df1_test['Revised'].astype('object')
+df1_test.dtypes
+
+#bytes consumed
+df1['Revised'].nbytes
+df1['Revised'].astype('category').nbytes#checking if datatype was 'category' then bytes consumed would be what
+#replace old value with new value 
+df1_test['Period'].replace(2020.06,2222.2,inplace=True)
+pd.unique(df1_test['Period'])
+#how many are null values
+df1.isnull().sum()
+df1_test.loc[:2,'Period'].replace(2222.2,0)
+df1_test['Period']=df1_test['Period'].astype('object')
+df1_test.loc[:2,'Period'].fillna('No data',inplace=True)#fills nan value with 'No data'
+df1_test.loc[:2,'Period'].replace(2222.2, np.nan,inplace=True)#convert particular dataset to Nan
+df1_test.loc[10:15,'Period'].replace({2222.2: None},inplace=True)#convert particular dataset to None
+df1_test.isnull().sum()#gives count of Nan values in each column
+
